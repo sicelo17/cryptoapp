fef6388eb1e4bc30fc55ec7fa3073a2312ff2751
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-11-14 10:16:17 +0200

    added input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
-
-//https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
-    <div className="App">
-     <h1>Apiiiii</h1>
+    <div className="coin__app">
+      <div className="coin__search">
+        <h1 className="coin__text">
+          Search a Currency
+        </h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin__input"/>
+        </form>
+      </div>
+
+      
     </div>
   );
 }
